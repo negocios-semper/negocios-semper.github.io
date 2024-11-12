db6d3c66073b54e322f764cb3b46ce710391fa3f
cristhiansemper <cristhiansemper@gmail.com>
2024-11-12 19:51:37 +0000

    gp-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import github from "@astrojs/github"; // Importa la integración de GitHub
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://www.semper.pe',
   base: '/', 
-  output: 'static',
-  integrations: [tailwind(), github()] , // Incluye la integración de GitHub aquí
+  output: 'static', // Esto asegura que se genere como un sitio estático
+  integrations: [tailwind()]
 });
